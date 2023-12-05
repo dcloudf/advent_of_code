1c7101d94b7e4cde676ed1f7e9e8432bdca6e0d4
Aleksandr Sizov <als.sizov@gmail.com>
2023-12-05 22:35:46 +0400

    Add the second part solution

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -2,11 +2,15 @@ use std::fs::read_to_string;
 
 fn main() {
     let (numbers, symbols) = parse_engine_schematic("./data/day3.txt");
-    println!("{}", filter_numbers(numbers, symbols).iter().sum::<u32>())
+    println!("{}", filter_numbers(&numbers, &symbols).iter().sum::<u32>());
+    println!("{}", calc_gear_ratios(symbols, numbers).iter().sum::<u32>());
 }
 
 #[derive(Debug, PartialEq)]
-struct Point(i32, i32);
+struct Symbol {
+    position: (i32, i32),
+    value: char,
+}
 
 #[derive(Debug, PartialEq)]
 struct Number {
@@ -16,11 +20,11 @@ struct Number {
     end: i32,
 }
 
-fn parse_engine_schematic(filename: &str) -> (Vec<Number>, Vec<Point>) {
+fn parse_engine_schematic(filename: &str) -> (Vec<Number>, Vec<Symbol>) {
     let file_text = read_to_string(filename).unwrap();
     let enum_lines = file_text.lines().enumerate();
     let mut numbers = Vec::new();
-    let mut symbol_positions = Vec::new();
+    let mut symbols = Vec::new();
     for enum_line in enum_lines {
         let mut enum_chars = enum_line.1.chars().enumerate();
         while let Some(char) = enum_chars.next() {
@@ -32,7 +36,10 @@ fn parse_engine_schematic(filename: &str) -> (Vec<Number>, Vec<Point>) {
                     if ch.1 == '.' {
                         break;
                     } else if !ch.1.is_ascii_digit() {
-                        symbol_positions.push(Point(ch.0 as i32, enum_line.0 as i32));
+                        symbols.push(Symbol {
+                            position: (ch.0 as i32, enum_line.0 as i32),
+                            value: ch.1,
+                        });
                         break;
                     }
                     digits.push(ch.1);
@@ -45,24 +52,61 @@ fn parse_engine_schematic(filename: &str) -> (Vec<Number>, Vec<Point>) {
                     end: end as i32,
                 });
             } else if char.1 != '.' {
-                symbol_positions.push(Point(char.0 as i32, enum_line.0 as i32))
+                symbols.push(Symbol {
+                    position: (char.0 as i32, enum_line.0 as i32),
+                    value: char.1,
+                })
             }
         }
     }
-    (numbers, symbol_positions)
+    (numbers, symbols)
 }
 
-fn filter_numbers(numbers: Vec<Number>, symbols: Vec<Point>) -> Vec<u32> {
+fn filter_numbers(numbers: &Vec<Number>, symbols: &Vec<Symbol>) -> Vec<u32> {
     numbers
         .into_iter()
         .filter(|num| {
             symbols.iter().any(|s| {
-                (num.start - 1 <= s.0 && s.0 <= num.end + 1)
-                    && (s.1 == num.row || s.1 == num.row - 1 || s.1 == num.row + 1)
+                (num.start - 1 <= s.position.0 && s.position.0 <= num.end + 1)
+                    && (s.position.1 == num.row
+                        || s.position.1 == num.row - 1
+                        || s.position.1 == num.row + 1)
             })
         })
         .map(|x| x.value)
         .collect()
+}
+
+fn calc_gear_ratios(symbols: Vec<Symbol>, numbers: Vec<Number>) -> Vec<u32> {
+    symbols
+        .iter()
+        .filter(|x| x.value == '*')
+        .filter(|s| {
+            numbers
+                .iter()
+                .filter(|num| {
+                    (num.start - 1 <= s.position.0 && s.position.0 <= num.end + 1)
+                        && (s.position.1 == num.row
+                            || s.position.1 == num.row - 1
+                            || s.position.1 == num.row + 1)
+                })
+                .count()
+                == 2
+        })
+        .map(|s| {
+            numbers
+                .iter()
+                .filter(|num| {
+                    (num.start - 1 <= s.position.0 && s.position.0 <= num.end + 1)
+                        && (s.position.1 == num.row
+                            || s.position.1 == num.row - 1
+                            || s.position.1 == num.row + 1)
+                })
+                .map(|x| x.value)
+                .reduce(|x, y| x * y)
+                .unwrap()
+        })
+        .collect::<Vec<u32>>()
 }
 
 #[cfg(test)]
@@ -70,16 +114,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_day3() {
+    fn test_day3_part1() {
         let (numbers, symbols) = parse_engine_schematic("./data/test_day3.txt");
-        assert_eq!(filter_numbers(numbers, symbols).iter().sum::<u32>(), 4361)
+        assert_eq!(filter_numbers(&numbers, &symbols).iter().sum::<u32>(), 4361)
+    }
+
+    #[test]
+    fn test_day3_part2() {
+        let (numbers, symbols) = parse_engine_schematic("./data/test_day3.txt");
+        assert_eq!(
+            calc_gear_ratios(symbols, numbers).iter().sum::<u32>(),
+            467835
+        )
     }
 
     #[test]
     fn test_filter_numbers() {
         assert_eq!(
             filter_numbers(
-                Vec::from([
+                &Vec::from([
                     Number {
                         value: 467,
                         row: 0,
@@ -93,7 +146,10 @@ mod tests {
                         end: 7
                     }
                 ]),
-                Vec::from([Point(3, 1)])
+                &Vec::from([Symbol {
+                    position: (3, 1),
+                    value: '*'
+                }])
             ),
             Vec::from([467,])
         )
